d448eee970a28c3fed9a3a02231efc94f109d465
turttlehouse <buidandrun464@gmail.com>
2024-08-31 13:39:04 +0545

    Tailwind CSS configured

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+//Task  1 make a navbar
+import React from 'react'
+
+const Navbar = () => {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Navbar here...
+    </h1>
+
+    </>
+  )
+}
+
+export default Navbar
